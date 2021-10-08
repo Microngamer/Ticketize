167a1d7453468ba808ec3e4df173587ceadfc189
XenKys <78646587+XenKys@users.noreply.github.com>
2021-10-08 14:03:28 +0200

    Ticketize - Updated (Fixed some bugs)

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -4,14 +4,17 @@ module.exports = {
     name: "eval",
     aliases: [],
     async execute(message, args, prefix) {
-        if (message.author.id != "658325242346864661" || !args.join(" ")) return
+        if (message.author.id != "658325242346864661") return send_error(message, "This command is avaible only my owner.")
 
-        var embed = new DiscordJS.MessageEmbed()
-        .addField("Output", `\`\`\`js\n${args.join(" ")}\n\`\`\``)
-        .addField("Input", `\`\`\`js\n${eval(args.join(" "))}\n\`\`\``)
-        .addField("Type", `\`\`\`js\n${typeof args.join(" ")}\n\`\`\``)
+        let evaled = args.join(" ")
+
+        if (!evaled) return send_error(message, "You dind't provide a thing to eval.")
 
         try {
+            var embed = new DiscordJS.MessageEmbed()
+            .addField("Output", `\`\`\`js\n${evaled}\n\`\`\``)
+            .addField("Input", `\`\`\`js\n${eval(evaled)}\n\`\`\``)
+            .addField("Type", `\`\`\`js\n${typeof evaled}\n\`\`\``)
             message.channel.send(embed)
         } catch (error) {
             message.channel.send(`\`\`\`js\n${error}\n\`\`\``)
