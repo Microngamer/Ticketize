e62ffc8fb30337fc7eb17b7f34919d95ebab7a5d
XenKys <78646587+XenKys@users.noreply.github.com>
2021-10-10 11:18:55 +0200

    Ticketize - Updated (Fixed some bugs)

--- commands/blacklist/blacklist-remove.js
+++ commands/blacklist/blacklist-remove.js
@@ -3,12 +3,14 @@ module.exports = {
     aliases: ["bl-rmv"],
     permission: "MANAGE_CHANNELS",
     async execute(message, args, prefix) {
-        let number = parseInt(args[0]) - 1
+        let number = args[0]
 
         if (!number) return send_error(message, "You didn't provide a number.")
 
         blacklists.findOne({ GuildId: message.guild.id }, async (err, data) => {
             if (data) {
+                number = parseInt(args[0]) - 1
+                
                 data.Users.splice(number, 1)
                 data.save()
 
