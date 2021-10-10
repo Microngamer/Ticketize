be71ab7a3cebfb80c7dffa4b0ec54133deaaf956
XenKys <78646587+XenKys@users.noreply.github.com>
2021-10-10 11:13:11 +0200

    Ticketize - Updated (Fixed some bugs)

--- commands/blacklist/blacklist-remove.js
+++ commands/blacklist/blacklist-remove.js
@@ -3,22 +3,16 @@ module.exports = {
     aliases: ["bl-rmv"],
     permission: "MANAGE_CHANNELS",
     async execute(message, args, prefix) {
-        let member = message.mentions.members.first()
+        let number = parseInt(args[1]) - 1
 
-        if (!member) {
-            member = message.guild.members.cache.find(user => user.user.username == args.join(" ")) || message.guild.members.cache.find(user => user.nickname == args.join(" ")) || message.guild.members.cache.find(user => user.user.tag == args[0]) || message.guild.members.cache.get(args[0])
-        }
-
-        if (!member) return send_error(message, "You didn't provide a member.")
+        if (!number) return send_error(message, "You didn't provide a number.")
 
         blacklists.findOne({ GuildId: message.guild.id }, async (err, data) => {
             if (data) {
-                if (!data.Users.includes(member.id)) return send_error(message, "This user is not in the blacklist.")
-
-                data.Users.splice(member.id, 1)
+                data.Users.splice(number, 1)
                 data.save()
 
-                send_correct(message, `${member.user.username} has been removed from the blacklist.`)
+                send_correct(message, `The user number ${number + 1} has been removed from the blacklist.`)
             } else {
                 return send_error(message, "In this server there isn't a blacklist.")
             }
