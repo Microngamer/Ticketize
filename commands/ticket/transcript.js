c677fba4a8cc7bb438e3944f6f518a5f4edf06cc
XenKys <78646587+XenKys@users.noreply.github.com>
2021-10-13 15:01:07 +0200

    Ticketize - Updated (Fixed some bugs)

--- commands/ticket/transcript.js
+++ commands/ticket/transcript.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute (message, args, prefix) {
         if (!message.channel.topic || !message.channel.topic.startsWith(`Tickets powered by ${client.user.username} | User ID:`)) return send_error(message, "This command is avaible only a ticket channel.")
 
-        DiscordTRANSCRIPT.fetchTranscript(message.channel, message, 100).then(data => {
+        DiscordTRANSCRIPT.fetchTranscript(message.channel, message, 99).then(data => {
             const file = new DiscordJS.MessageAttachment(data, `transcript-${message.channel.id}.html`)
             message.channel.send(`<:TicketizeMARK:883296061911871488> | **Succesfully saved transcript**`, file)
         })
