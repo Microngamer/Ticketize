4e7d4924812a1428e9571ca1d90662573d2fbe97
XenKys <78646587+XenKys@users.noreply.github.com>
2021-10-16 18:43:26 +0200

    Ticketize - Updated (Fixed some bugs)

--- commands/ticket/close.js
+++ commands/ticket/close.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute (message, args, prefix) {
         if (!message.channel.topic || !message.channel.topic.startsWith(`Tickets powered by ${client.user.username} | User ID:`)) return send_error(message, "This command is avaible only a ticket channel.")
 
-        send_correct(message, "The ticket will be close in a few seconds.")
+        message.channel.send("<:TicketizeMARK:883296061911871488> | **The ticket will be close in a few seconds.**")
 
         tickets.findOne({ GuildId: message.guild.id, UserId: message.channel.topic.slice(40), ChannelId: message.channel.id }, async (err, data) => {
             if (!data) return
