06534590ada3b6d54e12db77ab486cd1158fe960
XenKys <78646587+XenKys@users.noreply.github.com>
2021-10-16 17:58:01 +0200

    Ticketize - Updated (Now the ticket will be close in 5 seconds)

--- commands/ticket/close.js
+++ commands/ticket/close.js
@@ -5,12 +5,16 @@ module.exports = {
     async execute (message, args, prefix) {
         if (!message.channel.topic || !message.channel.topic.startsWith(`Tickets powered by ${client.user.username} | User ID:`)) return send_error(message, "This command is avaible only a ticket channel.")
 
-        tickets.findOne({ GuildId: message.guild.id, UserId: message.channel.topic.slice(40), ChannelId: message.channel.id }, async (err, data) => {
-            if (!data) return
+        send_correct(message, "The ticket will be close in a few seconds.")
 
-            await tickets.findOneAndDelete({ GuildId: message.guild.id, UserId: message.channel.topic.slice(40), ChannelId: message.channel.id })
-        })
-        
-        message.channel.delete()
+        setTimeout(() => {
+            tickets.findOne({ GuildId: message.guild.id, UserId: message.channel.topic.slice(40), ChannelId: message.channel.id }, async (err, data) => {
+                if (!data) return
+    
+                await tickets.findOneAndDelete({ GuildId: message.guild.id, UserId: message.channel.topic.slice(40), ChannelId: message.channel.id })
+            })
+            
+            message.channel.delete()
+        }, 5000)
     }
 }
